7e39de710342acf24fe073032ec0dfce0b50da52
Youssef-Mafa <youssefmafanze@gmail.com>
2023-08-16 18:43:26 -0700

    a

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
--- Show privileges
--- Show privileges for `user_0d_1`
-SHOW GRANTS FOR user_0d_1@localhost;
--- Show privileges for `user_0d_2`
-SHOW GRANTS FOR user_0d_2@localhost;
+-- show grants for users user_0d_1, user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,11 @@
--- Create a root user
--- Create a root user on localhost
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
+-- add user GRANT SELECT
+
+
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
