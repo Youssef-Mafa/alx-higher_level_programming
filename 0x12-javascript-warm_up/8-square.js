d501be12d5c02745c9c97a36916609038f56daca
Youssef-Mafa <youssefmafanze@gmail.com>
2023-12-11 07:47:58 -0800

    a

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
-const len = Number(process.argv[2]);
-if (isNaN(len)) {
-	console.log('Missing size');
-} else {
-	let mySqure;
-	for (let i = 0; i < len; i++) {
-		mySqure = '';
-		for (let j = 0; j < len; j++) {
-			mySqure += 'X';
-		}
-		console.log(mySqure);
+
+const squareSize = Number(process.argv[2]);
+
+if (Number(process.argv[2])) {
+	let i = 0;
+
+	for (i; i < squareSize; i++) {
+		console.log('X'.repeat(squareSize));
 	}
+} else {
+	console.log('Missing size');
 }
