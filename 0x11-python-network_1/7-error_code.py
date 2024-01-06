b7e6af67eef9019eb24d5f34265b19633575e270
Youssef-Mafa <youssefmafanze@gmail.com>
2024-01-06 14:07:23 -0800

    a

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''displays the body of the response and handel error'''
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = sys.argv[1]
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
