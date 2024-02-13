31b61ac6c427132062977e8b3f7085942d215744
Youssef-Mafa <125815717+Youssef-Mafa@users.noreply.github.com>
2024-02-13 17:08:02 +0100

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/1-writeme.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-// write data in a file from arg
-const fs = require('fs');
-
-if (process.argv.length > 3) {
-  fs.writeFile(process.argv[2], process.argv[3], (error) => {
-    if (error) console.log(error);
-  });
-}
